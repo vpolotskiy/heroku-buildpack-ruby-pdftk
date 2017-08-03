e89ce22d8c554b73ff8a1bcca8deb77526e647ac
Vitaliy Polotskiy <vpolotskiy@gmail.com>
2017-08-03 03:01:10 +0300

    Updated source url

--- lib/custom/pdftk.rb
+++ lib/custom/pdftk.rb
@@ -6,7 +6,7 @@ class Pdftk < BaseCustom
     "pdftk"
   end
   def source_url
-    "http://s3.amazonaws.com/source_url_here"
+    "https://s3-us-west-2.amazonaws.com/libre-doc-combiner-dev/pdftk.tar.gz"
   end
   def used?
     File.exist?("#{build_path}/bin/pdftk") && File.exist?("#{build_path}/bin/lib/libgcj.so.12")
@@ -14,15 +14,15 @@ class Pdftk < BaseCustom
   def compile
     write_stdout "compiling #{name}"
     #download the source and extract
-    %x{ mkdir -p #{path} && curl --silent #{source_url} -o - | tar -xz -C #{path} -f - } 
+    %x{ mkdir -p #{path} && curl --silent #{source_url} -o - | tar -xz -C #{path} -f - }
     write_stdout "complete compiling #{name}"
   end
   def cleanup!
-    
+
   end
   def prepare
     File.delete("#{build_path}/bin/lib/libgcj.so.12") if File.exist?("#{build_path}/bin/libgcj.so.12")
     File.delete("#{build_path}/bin/pdftk") if File.exist?("#{build_path}/bin/pdftk")
   end
-  
+
 end
